34edaed8a2d5b51017637435be5d4c2fc4f710cc
NChaturvedi18 <45910607+NChaturvedi18@users.noreply.github.com>
2026-06-21 23:55:44 +0530

    added shell bin command

--- ci/Scripts/notify.sh
+++ ci/Scripts/notify.sh
@@ -5,7 +5,9 @@ echo "Notifying stakeholders..."
 emailNotification() {
     echo "Sending email notification..."
         # Replace with your email sending command or script
-        echo "Build and deployment completed successfully." | mail -s "Notification" Neha.Chaturvedi181993.nc@gmail.com
+        mail to: 'Neha.Chaturvedi181993.nc@gmail.com',
+        subject: "Pipeline Success: ${env.JOB_NAME} [Build #${env.BUILD_NUMBER}]",
+        body: "Hi Neha,\n\nThe build and deployment completed successfully.\n\nView details here: ${env.BUILD_URL}"
 }
 
 emailNotification
