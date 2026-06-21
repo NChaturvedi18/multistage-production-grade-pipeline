51299459bd7e34476a5661c4a9998f37da2b728a
NChaturvedi18 <45910607+NChaturvedi18@users.noreply.github.com>
2026-06-21 17:32:44 +0530

    added Jenkinsfile & minimum script to test multistage job

---
+++ src/script.js
@@ -0,0 +1,7 @@
+// This is a single-line comment in JavaScript
+
+// 1. Create a variable storing text
+const val = "This script is a part of a multistage production-grade pipeline.";
+
+// 2. Output the text to the console screen
+console.log('val:', val);
